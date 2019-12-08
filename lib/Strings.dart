ee2d60ac8d68ef7a07c1ec20b7cc9437c689183a
Chrystian Salgado <salgado.dev@gmail.com>
2019-12-08 10:38:12 -0300

    fix: application problens

--- lib/Strings.dart
+++ lib/Strings.dart
@@ -1,4 +1,6 @@
 class Strings {
+  static const String main_screen_title = "Alcool ou Gasolina";
   static const String main_screen_subtitle = "Saiba qual a melhor opção para abastecer seu carro, se é com alcool ou gasolina.";
-  static const String main_alcool_placeholder = ""
+  static const String main_alcool_placeholder = "Preço do alcool: ex.: 3.49";
+  static const String main_gasolina_placeholder = "Preço do gasolina: ex.: 3.59";
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,10 @@
 import 'package:flutter/material.dart';
-import 'Strings.dart'
+import 'Strings.dart';
 
-void main() => runApp(MainScreen());
-
-class MainScreen extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: Text("Alcool ou Gasolina"),
-      color: Colors.lightGreen,
+void main() {
+  runApp(MaterialApp(
       home: MainPage(),
-    );
-  }
+    ));
 }
 
 class MainPage extends StatefulWidget {
@@ -21,30 +13,43 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+
+  // setup alcool controller
+  TextEditingController _alcoolEditingController = TextEditingController();
+
+  // setup gasolina controller
+  TextEditingController _gasolinaEditingController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: SingleChildScrollView(
-        padding: EdgeInsets.all(8),
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(4),
-              child: Text(Strings.main_screen_subtitle),
-            ),
-            Padding(
-              padding: EdgeInsets.all(4),
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: Text("")
-                ),
-                cursorColor: Colors.black,
-                maxLength: 5,
-                keyboardType: TextInputType.number,
-                controller: ,
+    return Scaffold(
+      appBar: AppBar(
+          title: Text(Strings.main_screen_title),
+          backgroundColor: Colors.green
+      ),
+      body: Container(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(8),
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(4),
+                child: Text(Strings.main_screen_subtitle),
               ),
-            ),
-          ],
+              Padding(
+                padding: EdgeInsets.all(4),
+                child: TextField(
+                  decoration: InputDecoration(
+                      labelText: Strings.main_screen_subtitle
+                  ),
+                  cursorColor: Colors.black,
+                  maxLength: 5,
+                  keyboardType: TextInputType.number,
+                  controller: _alcoolEditingController,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
