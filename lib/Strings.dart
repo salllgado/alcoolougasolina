d084d21a4110f78d15032e29c8ac60a0ebae68c4
Chrystian Salgado <salgado.dev@gmail.com>
2019-12-08 12:05:59 -0300

    feat: add HEX extension and handler gesture to dismiss

--- lib/Strings.dart
+++ lib/Strings.dart
@@ -3,4 +3,5 @@ class Strings {
   static const String main_screen_subtitle = "Saiba qual a melhor opção para abastecer seu carro, se é com alcool ou gasolina.";
   static const String main_alcool_placeholder = "Preço do alcool: ex.: 3.49";
   static const String main_gasolina_placeholder = "Preço do gasolina: ex.: 3.59";
+  static const String main_button_action = "Calcular";
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'Strings.dart';
+import 'ColorsExtension.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -20,38 +21,100 @@ class _MainPageState extends State<MainPage> {
   // setup gasolina controller
   TextEditingController _gasolinaEditingController = TextEditingController();
 
+  void handlerButtonTapped() {
+    print(_gasolinaEditingController.text);
+  }
+
+  void handlerText(String text) {
+//    if (text.length > 3) {
+//      if (text.contains('.') == false ) {
+//        _gasolinaEditingController.text = '.$text';
+//      }
+//    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Text(Strings.main_screen_title),
+          title: Text(
+            Strings.main_screen_title,
+            style: TextStyle(
+              color: HexColor("ebc200")
+            ),
+          ),
           backgroundColor: Colors.green
       ),
       body: Container(
-        child: SingleChildScrollView(
-          padding: EdgeInsets.all(8),
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.all(4),
-                child: Text(Strings.main_screen_subtitle),
-              ),
-              Padding(
-                padding: EdgeInsets.all(4),
-                child: TextField(
-                  decoration: InputDecoration(
-                      labelText: Strings.main_screen_subtitle
+        child: GestureDetector(
+          onTap: (){
+            FocusScope.of(context).requestFocus(new FocusNode());
+          },
+          child: Container(
+            child: SingleChildScrollView(
+              padding: EdgeInsets.all(8),
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Text(
+                      Strings.main_screen_subtitle,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20
+                      ),
+                    ),
                   ),
-                  cursorColor: Colors.black,
-                  maxLength: 5,
-                  keyboardType: TextInputType.number,
-                  controller: _alcoolEditingController,
-                ),
+                  Padding(
+                    padding: EdgeInsets.all(4),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          labelText: Strings.main_alcool_placeholder,
+                          suffixStyle: TextStyle(
+                            fontSize: 16,
+                          )
+                      ),
+                      cursorColor: Colors.black,
+                      maxLength: 5,
+                      keyboardType: TextInputType.number,
+                      controller: _alcoolEditingController,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(4),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          labelText: Strings.main_gasolina_placeholder,
+                          suffixStyle: TextStyle(
+                            fontSize: 16,
+                          )
+                      ),
+                      cursorColor: Colors.black,
+                      maxLength: 5,
+                      keyboardType: TextInputType.number,
+                      maxLengthEnforced: true,
+                      onChanged: handlerText,
+                      controller: _gasolinaEditingController,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 44,
+                    width: 288,
+                    child: FlatButton(
+                      child: Text(Strings.main_button_action),
+                      textColor: HexColor("ebc200"),
+                      color: Colors.green,
+                      textTheme: ButtonTextTheme.primary,
+                      onPressed: handlerButtonTapped,
+                    ),
+                  )
+                ],
               ),
-            ],
+            ),
           ),
         ),
-      ),
+      )
     );
   }
 }
